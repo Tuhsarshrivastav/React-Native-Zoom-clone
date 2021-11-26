a8df3dedb9269b690215db5fe055571d1feddd4a
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 <ts3657@gmail.com>
2021-11-26 10:59:07 +0530

    contactMenu completed

--- components/ContactMenu.jsx
+++ components/ContactMenu.jsx
@@ -1,15 +1,43 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
+
+const contactMenus = [
+  {
+    type: "starred",
+    name: "Starred",
+  },
+  {
+    type: "contact",
+    name: "kendall jenner",
+    photo: require("../assets/Kendall-Jenner.webp"),
+  },
+  {
+    type: "contact",
+    name: "Zyan Malik",
+    photo: require("../assets/zayn1.webp"),
+  },
+  {
+    type: "contact",
+    name: "post malone",
+    photo: require("../assets/post_malone_image_3_2_.jpg"),
+  },
+];
 const ContactMenu = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.staredIcon}>
-          <AntDesign name="star" size={30} color="#efefef" />
+      {contactMenus.map((contact, index) => (
+        <View key={index} style={styles.row}>
+          {contact.type == "starred" ? (
+            <View style={styles.staredIcon}>
+              <AntDesign name="star" size={30} color="#efefef" />
+            </View>
+          ) : (
+            <Image source={contact.photo} style={styles.image} />
+          )}
+          <Text style={styles.text}>{contact.name}</Text>
         </View>
-        <Text style={styles.text}>Starred</Text>
-      </View>
+      ))}
     </View>
   );
 };
@@ -34,6 +62,11 @@ const styles = StyleSheet.create({
     height: 55,
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 20,
+  },
+  image: {
+    width: 55,
+    height: 55,
     borderRadius: 20,
   },
 });
